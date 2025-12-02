0375629b431fd52c906fba9983e4f923edc995f5
leviomagbemi <leviomagbemi15@gmail.com>
2025-12-02 10:42:40 +0100

    Dynamic header and footer

---
+++ src/js/utils.mjs
@@ -0,0 +1,20 @@
+export async function loadTemplate(path){
+  const file = await fetch(path);
+  const result = await file.text();
+  return result;
+}
+
+export function renderWithTemplate(template, parentElement) {
+  parentElement.innerHTML = template;
+}
+
+export async function loadHeaderFooter(){
+  const headerContent = await loadTemplate('../partials/header.html');
+  const footerContent = await loadTemplate('../partials/footer.html');
+
+  const header = document.getElementById('main-header');
+  const footer = document.getElementById('main-footer');
+
+  renderWithTemplate(headerContent, header);
+  renderWithTemplate(footerContent, footer);
+}
